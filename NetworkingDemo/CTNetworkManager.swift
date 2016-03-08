5daea9104a938ea0640640abad5492f061f50863
Jubin Jacob <you@example.com>
2016-03-08 08:10:16 +0530

    Added an extension for network manager. Going the swift way

---
+++ NetworkingDemo/CTNetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  CTNetworkManager.swift
+//  Catapult
+//
+//  Created by Jubin Jacob on 08/03/16.
+//  Copyright © 2016 J. All rights reserved.
+//
+
+/**
+Add functionality to Network Manager here
+
+*/
+
+import Foundation
+
+extension NetworkManager {
+    
+}
